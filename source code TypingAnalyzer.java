e89750a49be053d42a081f95a159b9bde0da0e27
gopi-10-10 <gopikrishna.m2004@gmail.com>
2025-09-09 21:19:44 +0530

    Create source code TypingAnalyzer.java

---
+++ source code TypingAnalyzer.java
@@ -0,0 +1,9 @@
+package com.typinganalyzer;
+
+import javax.swing.*;
+
+public class TypingAnalyzer {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(MainMenu::new);
+    }
+}
